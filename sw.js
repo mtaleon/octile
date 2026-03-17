6f29e1087d3378504253e4ae3cdb819c43ba2c15
oouyang <owen.ouyang@live.com>
2026-03-17 12:46:57 +0800

    Add full PWA support for PWABuilder packaging
    - Create manifest.json with icons array (48-512px), standalone display, portrait orientation
    - Generate PNG icons at 10 sizes from favicon.svg
    - Add manifest link, apple-mobile-web-app meta tags, and apple-touch-icon to index.html
    - Register service worker in init block
    - Update sw.js cache to v2 with manifest and key icons

--- sw.js
+++ sw.js
@@ -1,11 +1,14 @@
 'use strict';
 
-const CACHE_NAME = 'octile-v1';
+const CACHE_NAME = 'octile-v2';
 
 const STATIC_ASSETS = [
     '/',
     '/index.html',
-    '/favicon.svg'
+    '/favicon.svg',
+    '/manifest.json',
+    '/icons/icon-192.png',
+    '/icons/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
